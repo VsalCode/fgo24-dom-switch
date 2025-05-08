f15fa8465089ca3f5d940211e1c36011b467bbf9
VsalCode <vsal.ilham@gmail.com>
2025-05-08 13:25:50 +0700

    refactor: switch drinks logic

--- index.js
+++ index.js
@@ -7,34 +7,36 @@ const gelas2 = document.querySelector(".gelas > div:nth-child(2)");
 const gelas3 = document.querySelector(".gelas > div:last-child");
 
 tombol1.addEventListener("click", () => {
-  if (!gelas2.classList.contains("kosong")) {
-    alert("Pindahkan Dulu Gelas 2 ke Gelas Kosong !");
-  } else if (gelas3.classList.contains("kopi")) {
+   if (gelas3.classList.contains("kopi")) {
     gelas2.classList.replace("kosong", "teh");
     gelas1.classList.replace("teh", "kosong");
-  } else {
+  } else if (gelas3.classList.contains("teh")) {
     gelas2.classList.replace("kosong", "kopi");
     gelas1.classList.replace("kopi", "kosong");
+  } else {
+    alert("Pindahkan Dulu Gelas 2 ke Gelas Kosong !");
   }
 });
 
 tombol2.addEventListener("click", () => {
-  if (!gelas3.classList.contains("kosong")) {
-    alert("Pindahkan Dulu Gelas 3 ke Gelas Kosong !");
-    gelas2.classList.replace("kosong", "teh");
-  }
   if (gelas1.classList.contains("teh")) {
     gelas3.classList.replace("kosong", "kopi");
     gelas2.classList.replace("kopi", "kosong");
+  } else if (gelas1.classList.contains("kopi")){
+    gelas3.classList.replace("kosong", "teh");
+    gelas2.classList.replace("teh", "kosong");
+  } else {
+    alert("Pindahkan Dulu Gelas 3 ke Gelas Kosong !");
   }
-  gelas3.classList.replace("kosong", "teh");
-  gelas2.classList.replace("teh", "kosong");
 });
 
 tombol3.addEventListener("click", () => {
-  if (gelas1.classList.contains("kosong")) {
+  if (gelas2.classList.contains("kopi")) {
     gelas1.classList.replace("kosong", "teh");
     gelas3.classList.replace("teh", "kosong");
+  } else if (gelas2.classList.contains("teh")){
+    gelas1.classList.replace("kosong", "kopi");
+    gelas3.classList.replace("kopi", "kosong");
   } else {
     alert("Pindahkan dulu gelas 1 ke gelas kosong!");
   }
